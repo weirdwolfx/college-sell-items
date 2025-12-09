255a8a2bf359110e3c41111c6a1ba55d099da173
weirdwolfx <aadityapgoyal@gmail.com>
2025-12-09 22:36:23 +0530

    Deployment commit

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer>
-            <p>Copyright <span>&copy;</span> <a href="https://github.com/weirdwolfx">Aaditya Goyal</a> 2025</p>
-        </footer>
-    )
-}
--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './App.jsx'
-
-const root = createRoot(document.getElementById('root'))
-root.render(
-	<App />
-)
